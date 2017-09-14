f97f855973c1a983cf69cc9134ed0fb01e56ab94
vincen7su <31243646+vincen7su@users.noreply.github.com>
2017-09-14 23:44:26 +0800

    Replace selenium-download with webdriver-manager.

--- test/e2e/ruten.js
+++ test/e2e/ruten.js
@@ -1,11 +1,8 @@
-var config = require('../../nightwatch.conf.js');
-
 module.exports = {
   'Ruten Assert Title': function(browser) {
     browser.url('http://www.ruten.com.tw')
            .waitForElementVisible('body')
            .assert.title('露天拍賣-台灣 NO.1 拍賣網站')
-	   .saveScreenshot('ruten.png')
            .end();
   }
 };
